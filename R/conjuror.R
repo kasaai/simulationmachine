46e937df92358a84260f8de0e3fe69b5ab8ae4cc
kevinykuo <kevinykuo@gmail.com>
2019-08-19 11:56:42 -0400

    implement conjure

--- R/conjuror.R
+++ R/conjuror.R
@@ -17,3 +17,38 @@ simulation_machine <- function(num_records,
     inflation = inflation
   )
 }
+
+#' Simulate Claim Histories
+#' 
+#' @importFrom conjuror conjure
+#' @param sd_claim Value of the standard deviation used in the log-normal 
+#'   distribution of the claim sizes.
+#' @param sd_recovery  Value of the standard deviation used in the log-normal 
+#'   distribution of the recovery sizes.
+#' @param seed Seed for random number generation.
+#' @param rows_per_partition (Optional) For parallel processing, number of 
+#'   observations that are treated at the same time.
+#' @export
+conjure.simulation_machine <- function(incantation, sd_claim, sd_recovery,
+                                       seed = NULL, rows_per_partition = NULL, ...) {
+  features <- Feature.Generation(
+    V = incantation[["num_records"]],
+    LoB.dist = incantation[["lob_distribution"]],
+    inflation = incantation[["inflation"]],
+    seed = seed
+  )
+  
+  if (is.null(rows_per_partition)) {
+    rows_per_partition <- nrow(features)
+  }
+  
+  records <- Simulation.Machine(
+    features,
+    npb = rows_per_partition,
+    seed1 = seed,
+    std1 = sd_claim,
+    std2 = sd_recovery
+  )
+  
+  records
+}
